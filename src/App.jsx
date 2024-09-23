3b9cb2e41e2c81a452462c5c72a6133194c287dc
Melchor Callos <melchorcallos04@gmail.com>
2024-09-23 18:01:36 +0800

    Update: Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Home from "./pages/Home";
@@ -6,6 +6,7 @@ import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
+import Preloader from "./pages/Preloader";
 import NavBar from "./components/NavBar";
 import DownloadButton from "./components/DownloadButton";
 import SocialMenu from "./components/SocialMenu";
@@ -13,25 +14,37 @@ import Background from "./components/Background";
 
 function App() {
   const location = useLocation();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
+    {loading ? (
+        <Preloader />
+      ) : (
+        
       <div className="w-screen h-screen flex flex-col items-center overflow-hidden">
-        <NavBar />
-        <AnimatePresence mode="wait">
-          <Routes location={location} key={location.pathname}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/experience" element={<Experience />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-          //{" "}
-        </AnimatePresence>
-        <SocialMenu />
-        <DownloadButton />
-        <Background />
-      </div>
+      <NavBar />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+        //{" "}
+      </AnimatePresence>
+      <SocialMenu />
+      <DownloadButton />
+      <Background />
+    </div>
+      )}
     </>
   );
 }
---
+++ src/pages/Preloader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { motion } from "framer-motion";
+import { contents } from "../assets/contents/loader";
+
+const Preloader = () => {
+  return (
+    <motion.div
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 1 }}
+      className="w-screen h-screen flex flex-col justify-center items-center bg-light dark:bg-dark"
+    >
+      <img src={contents.loader} className="w-40" />
+      <h1 className="xs:text-3xl text-2xl font-bold text-primary flex flex-col justify-center items-center text-center animate-pulse">
+        {contents.greetings.toUpperCase()}
+      </h1>
+    </motion.div>
+  );
+};
+
+export default Preloader;
