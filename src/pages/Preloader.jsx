6ac382542ab5196bca8d3906e8b930946b1253f8
Melchor Callos <melchorcallos04@gmail.com>
2024-09-24 10:09:33 +0800

    Update Preloader.jsx

--- src/pages/Preloader.jsx
+++ src/pages/Preloader.jsx
@@ -11,7 +11,7 @@ const Preloader = () => {
       className="w-screen h-screen flex flex-col justify-center items-center bg-light dark:bg-dark"
     >
       <img src={contents.loader} className="w-40" />
-      <h1 className="xs:text-3xl text-2xl font-bold text-primary flex flex-col justify-center items-center text-center animate-pulse">
+      <h1 className="flex flex-col justify-center items-center xs:text-3xl text-2xl font-bold text-primary text-center animate-pulse">
         {contents.greetings.toUpperCase()}
       </h1>
     </motion.div>
