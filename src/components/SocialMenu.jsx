92081b80bb99631f7a121d74daf714db4294fa2b
Melchor Callos <melchorcallos04@gmail.com>
2024-10-01 14:50:56 +0800

    Update SocialMenu.jsx

--- src/components/SocialMenu.jsx
+++ src/components/SocialMenu.jsx
@@ -5,21 +5,6 @@ import { links } from "../assets/contents/about";
 const SocialMenu = () => {
   const [openSocialMenu, setOpenSocialMenu] = useState(false);
 
-  const RoundButton = ({ children, href, onClick }) => {
-    return (
-      <a
-        href={href}
-        onClick={onClick}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="size-10 flex justify-center items-center rounded-full bg-primary
-        text-light dark:text-dark text-xl hover:scale-125 duration-300 m-2"
-      >
-        {children}
-      </a>
-    );
-  };
-
   return (
     <div className="fixed bottom-0 left-0 z-50 flex md:flex-col flex-row-reverse m-2">
       <div
@@ -44,6 +29,21 @@ const SocialMenu = () => {
         <FaPlus />
       </RoundButton>
     </div>
+  );
+};
+
+const RoundButton = ({ children, href, onClick }) => {
+  return (
+    <a
+      href={href}
+      onClick={onClick}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="size-10 flex justify-center items-center rounded-full bg-primary
+        text-light dark:text-dark text-xl hover:scale-125 duration-300 m-2"
+    >
+      {children}
+    </a>
   );
 };
 
