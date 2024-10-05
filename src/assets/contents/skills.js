864242244bcb7cf87a6e3119fca0089b6d1a188b
Melchor Callos <melchorcallos04@gmail.com>
2024-10-05 09:20:42 +0800

    Update skills.js

--- src/assets/contents/skills.js
+++ src/assets/contents/skills.js
@@ -25,29 +25,59 @@ import arduino from "../images/skill/arduino.png";
 import photoshop from "../images/skill/photoshop.png";
 
 export const skills = [
-  { name: "C", img: c },
-  { name: "C++", img: cplusplus },
-  { name: "C#", img: csharp },
-  { name: "Java", img: java },
-  { name: "JavaScript", img: javascript },
-  { name: "PHP", img: php },
-  { name: "Selenium", img: selenium },
-  { name: "Appium", img: appium },
-  { name: "Cucumber", img: cucumber },
-  { name: "Apache Maven", img: apachemaven },
-  { name: "HTML5", img: html5 },
-  { name: "CSS3", img: css3 },
-  { name: "Bootstrap", img: bootstrap },
-  { name: "Material UI", img: materialui },
-  { name: "Tailwind CSS", img: tailwindcss },
-  { name: "Framer Motion", img: framermotion },
-  { name: "Vite", img: vite },
-  { name: "React", img: react },
-  { name: "Express", img: express },
-  { name: "Node", img: node },
-  { name: "MongoDB", img: mongodb },
-  { name: "MySQL", img: mysql },
-  { name: "Git", img: git },
-  { name: "Arduino", img: arduino },
-  { name: "Photoshop", img: photoshop },
+  { name: "C", img: c, level: 85, color: "bg-blue-600" },
+  { name: "C++", img: cplusplus, level: 90, color: "bg-blue-700" },
+  { name: "C#", img: csharp, level: 80, color: "bg-purple-600" },
+  { name: "Java", img: java, level: 75, color: "bg-red-600" },
+  { name: "JavaScript", img: javascript, level: 85, color: "bg-yellow-500" },
+  { name: "PHP", img: php, level: 70, color: "bg-indigo-600" },
+  { name: "Selenium", img: selenium, level: 80, color: "bg-green-600" },
+  { name: "Appium", img: appium, level: 75, color: "bg-teal-600" },
+  { name: "Cucumber", img: cucumber, level: 70, color: "bg-green-700" },
+  { name: "Apache Maven", img: apachemaven, level: 65, color: "bg-red-700" },
+  { name: "HTML5", img: html5, level: 90, color: "bg-orange-500" },
+  { name: "CSS3", img: css3, level: 85, color: "bg-blue-500" },
+  { name: "Bootstrap", img: bootstrap, level: 80, color: "bg-purple-500" },
+  { name: "Material UI", img: materialui, level: 75, color: "bg-blue-400" },
+  { name: "Tailwind CSS", img: tailwindcss, level: 85, color: "bg-teal-500" },
+  { name: "Framer Motion", img: framermotion, level: 70, color: "bg-pink-500" },
+  { name: "Vite", img: vite, level: 65, color: "bg-yellow-600" },
+  { name: "React", img: react, level: 90, color: "bg-blue-400" },
+  { name: "Express", img: express, level: 75, color: "bg-gray-700" },
+  { name: "Node", img: node, level: 80, color: "bg-green-700" },
+  { name: "MongoDB", img: mongodb, level: 70, color: "bg-green-500" },
+  { name: "MySQL", img: mysql, level: 75, color: "bg-blue-600" },
+  { name: "Git", img: git, level: 85, color: "bg-orange-600" },
+  { name: "Arduino", img: arduino, level: 65, color: "bg-blue-800" },
+  { name: "Photoshop", img: photoshop, level: 70, color: "bg-blue-300" },
+];
+
+export const programming = [
+  { name: "C", img: c, level: 60, color: "bg-blue-600" },
+  { name: "C++", img: cplusplus, level: 75, color: "bg-blue-700" },
+  { name: "C#", img: csharp, level: 80, color: "bg-purple-600" },
+  { name: "Java", img: java, level: 90, color: "bg-red-600" },
+  { name: "JavaScript", img: javascript, level: 80, color: "bg-yellow-500" },
+];
+
+export const testing = [
+  { name: "Selenium", img: selenium, level: 85, color: "bg-green-600" },
+  { name: "Appium", img: appium, level: 75, color: "bg-teal-600" },
+  { name: "Cucumber", img: cucumber, level: 85, color: "bg-green-700" },
+  { name: "Apache Maven", img: apachemaven, level: 60, color: "bg-red-700" },
+];
+
+export const webdev = [
+  { name: "Tailwind CSS", img: tailwindcss, level: 85, color: "bg-teal-500" },
+  { name: "React", img: react, level: 90, color: "bg-blue-400" },
+  { name: "Express", img: express, level: 60, color: "bg-gray-400" },
+  { name: "Node", img: node, level: 60, color: "bg-green-700" },
+  { name: "MongoDB", img: mongodb, level: 60, color: "bg-green-500" },
+];
+
+export const others = [
+  { name: "MySQL", img: mysql, level: 60, color: "bg-blue-600" },
+  { name: "Git", img: git, level: 85, color: "bg-orange-600" },
+  { name: "Arduino", img: arduino, level: 65, color: "bg-blue-800" },
+  { name: "Photoshop", img: photoshop, level: 65, color: "bg-blue-300" },
 ];
