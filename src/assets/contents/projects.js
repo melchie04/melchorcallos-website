8be0919588fbd59bc20737257513ce3af1c7f488
Melchor Callos <melchorcallos04@gmail.com>
2024-10-19 09:24:59 +0800

    Update projects.js

--- src/assets/contents/projects.js
+++ src/assets/contents/projects.js
@@ -6,32 +6,40 @@ import chowviwebsite from "../images/project/chowviwebsite.jpg";
 export const projects = [
   {
     name: "EatWell",
-    category: "React Website Development",
-    details: "A food recipe website with delicious meals, ingredients, step-by-step instructions, and helpful videos.",
+    category: "Website Development",
+    tools: ["React", "JavaScript"],
+    details:
+      "A food recipe website with delicious meals, ingredients, step-by-step instructions, and helpful videos.",
     github: "https://github.com/melchie04/food-recipe-website",
     demo: "https://melchie04-eatwell.netlify.app/",
     image: eatwellwebsite,
   },
   {
     name: "GifShare",
-    category: "React Website Development",
-    details: "A GIF sharing website where users can post animated images on social media platforms like Facebook and Twitter.",
+    category: "Website Development",
+    tools: ["React", "JavaScript"],
+    details:
+      "A GIF sharing website where users can post animated images on social media platforms like Facebook and Twitter.",
     github: "https://github.com/melchie04/gif-sharing-website",
     demo: "https://melchie04-gifshare.netlify.app/",
     image: gifsharewebsite,
   },
   {
     name: "Online Palaba",
-    category: "React Website Development",
-    details: "A landing page for a laundry and logistics service, featuring a convenient mobile app for customers.",
+    category: "Website Development",
+    tools: ["React", "JavaScript"],
+    details:
+      "A landing page for a laundry and logistics service, featuring a convenient mobile app for customers.",
     github: "https://github.com/melchie04/online-palaba-website",
     demo: "https://melchie04-online-palaba.netlify.app/",
     image: onlinepalabawebsite,
   },
   {
     name: "ChowVi",
-    category: "React Website Development",
-    details: "A landing page for a food delivery and fast food service, offering a convenient and delightful dining experience at your fingertips.",
+    category: "Website Development",
+    tools: ["React", "JavaScript"],
+    details:
+      "A landing page for a food delivery and fast food service, offering a convenient and delightful dining experience at your fingertips.",
     github: "https://github.com/melchie04/chowvi-website",
     demo: "https://melchie04-chowvi.netlify.app/",
     image: chowviwebsite,
